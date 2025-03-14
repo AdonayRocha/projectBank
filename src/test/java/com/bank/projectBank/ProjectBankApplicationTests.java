524e0ecd1aea04cc77afe7dcedc2a6a14fdeb0c9
adonayrocha <adonay.rodrigues.rocha@gmail.com>
2025-03-14 16:22:26 -0300

    first commit

---
+++ src/test/java/com/bank/projectBank/ProjectBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.projectBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
