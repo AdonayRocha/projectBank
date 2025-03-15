e89125aed02f4868b54a73e29fbb383f3d541885
adonayrocha <adonay.rodrigues.rocha@gmail.com>
2025-03-14 21:44:10 -0300

    PackagePom

---
+++ src/main/java/com/bank/projectBank/Model/Conta.java
@@ -0,0 +1,13 @@
+package com.bank.projectBank.Model;
+
+import java.security.Timestamp;
+
+public class Conta {
+    int numeroConta;
+    int numeroAgencia;
+    String nomeTitular;
+    String cpfTitular;
+    Timestamp dataAbertura;
+    Boolean ativo; // Ativo = True/ Desativa = False
+
+}
